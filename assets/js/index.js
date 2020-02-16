7abe6f4153d86706aa6f7258910b420275474340
jey <jey@jeys-MacBook-Pro.local>
2020-02-17 00:23:30 +0300

    add about page

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+
+var menu = document.getElementById('mobile-menu');
+
+function toggleMenu() {
+    menu.style.visibility = "visible";
+    menu.style.opacity = 1;
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+    menu.style.visibility = "hidden";
+    menu.style.opacity = 0;
+}
+window.onclick = function (event) {
+    if (event.target == menu) {
+    menu.style.visibility = "hidden";
+    menu.style.opacity = 0;
+    }
+}
+
+let loader_holder = document.getElementsByClassName("loader-holder")[0];
+
+function hideLoader() {
+    document.getElementById("loader").style.display = "none";
+    loader_holder.style.display = "none";
+}
