b856f66ee05f392107e25ca8cbb88f8c2452612b
jey <jey@jeys-MacBook-Pro.local>
2020-03-10 10:57:05 +0300

    Make changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,8 +13,8 @@ span.onclick = function () {
 }
 window.onclick = function (event) {
     if (event.target == menu) {
-    menu.style.visibility = "hidden";
-    menu.style.opacity = 0;
+        menu.style.visibility = "hidden";
+        menu.style.opacity = 0;
     }
 }
 
@@ -23,4 +23,22 @@ let loader_holder = document.getElementsByClassName("loader-holder")[0];
 function hideLoader() {
     document.getElementById("loader-holder").style.display = "none";
     loader_holder.style.display = "none";
+}
+
+
+/**
+ * Career body open close
+ */
+function closeBody() {
+    var menu = document.getElementById('career-body');
+    var detail = document.getElementById('more-detail');
+    
+    console.log(menu.style.display)
+    if(menu.style.display === "block"){
+        detail.innerHTML = "More Detail";
+        menu.style.display = "none";
+        return;
+    }
+    detail.innerHTML = "Less Detail";
+    menu.style.display = "block";
 }
